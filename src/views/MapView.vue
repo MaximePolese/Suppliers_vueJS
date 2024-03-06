ca9a0b1d88bb728879a2e4c75c2ec60df8eecab7
maxime.polese <maxime.polese@le-campus-numerique.fr>
2024-03-06 12:01:20 +0100

    carte ok

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,11 +1,32 @@
+<script setup>
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+import L from 'leaflet'
+import { ref } from 'vue'
+
+const zoom = ref(10)
+</script>
+
 <template>
   <div class="map">
     <h1>Carte des fournisseurs</h1>
+    <div class="leflet-map">
+      <l-map ref="map" v-model:zoom="zoom" :center="[45.18486504179179,  5.731181509376984]">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+      </l-map>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .map {
   margin: 0 2rem;
+}
+
+.leflet-map {
+  height: 700px;
 }
 </style>
